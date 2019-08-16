b0326aecd57632f18b37e0fe3dfb66083081a452
Barbara Peric <b.peric10@gmail.com>
2019-08-16 12:20:55 +0200

    Solve duplicate migration

--- db/migrate/20190815134859_add_coordinates_to_superheros.rb
+++
@@ -1,6 +0,0 @@
-class AddCoordinatesToSuperheros < ActiveRecord::Migration[5.2]
-  def change
-    add_column :superheros, :latitude, :float
-    add_column :superheros, :longitude, :float
-  end
-end
